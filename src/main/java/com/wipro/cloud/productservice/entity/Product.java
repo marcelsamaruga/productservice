17ec1ad5ee12e8d8d1d34085efd290c45bab683a
Marcel Samaruga da Costa <marcelsamaruga@gmail.com>
2019-10-02 23:56:59 -0300

    Initial commit

--- src/main/java/com/wipro/cloud/productservice/entity/Product.java
+++ src/main/java/com/wipro/cloud/productservice/entity/Product.java
@@ -20,7 +20,7 @@ public class Product {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
